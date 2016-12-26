2025a39c9aacdaf11ebaf33f97551f32d3725e8c
romainmenke <romainmenke@gmail.com>
2016-12-26 21:53:16 +0100

    example in readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 WIP adaptive http2 Pusher
+
+example :
+
+```go
+package main
+
+import (
+	"net/http"
+
+	"github.com/romainmenke/pusher"
+)
+
+func main() {
+
+	http.HandleFunc("/",
+		pusher.Pusher(http.FileServer(http.Dir("./cmd/static")).ServeHTTP),
+	)
+
+	err := http.ListenAndServeTLS(":4430", "cmd/localhost.crt", "cmd/localhost.key", nil)
+	if err != nil {
+		panic(err)
+	}
+
+}
+```
--- cmd/main.go
+++ cmd/main.go
@@ -8,15 +8,13 @@ import (
 
 func main() {
 
-	http.HandleFunc("/", pusher.Pusher(Static()))
+	http.HandleFunc("/",
+		pusher.Pusher(http.FileServer(http.Dir("./cmd/static")).ServeHTTP),
+	)
 
 	err := http.ListenAndServeTLS(":4430", "cmd/localhost.crt", "cmd/localhost.key", nil)
 	if err != nil {
 		panic(err)
 	}
 
-}
-
-func Static() func(w http.ResponseWriter, r *http.Request) {
-	return http.FileServer(http.Dir("./cmd/static")).ServeHTTP
 }
