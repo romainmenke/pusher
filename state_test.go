03e5a97078ad3b21ba6cf3f6f07d32435f86259c
romainmenke <romainmenke@gmail.com>
2016-12-29 17:19:11 +0100

    more accurate bench tests

--- state_test.go
+++ state_test.go
@@ -8,6 +8,20 @@ import (
 )
 
 func BenchmarkAddToPushMap(b *testing.B) {
+	pushMap = make(map[string]map[string]*push)
+
+	for n1 := 0; n1 < 100; n1++ {
+		for n2 := 0; n2 < 5; n2++ {
+			r := &http.Request{
+				RequestURI: "/page-" + fmt.Sprint(n1),
+				Header: http.Header{
+					PushInitiatorKey: []string{"/site-" + fmt.Sprint(n2)},
+				},
+			}
+			addToPushMap(r)
+		}
+	}
+
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
@@ -23,6 +37,8 @@ func BenchmarkAddToPushMap(b *testing.B) {
 }
 
 func BenchmarkReadFromPushMap(b *testing.B) {
+	pushMap = make(map[string]map[string]*push)
+
 	for n1 := 0; n1 < 100; n1++ {
 		for n2 := 0; n2 < 5; n2++ {
 			r := &http.Request{
