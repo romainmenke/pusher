76665b923475b72fdab272018a8e64db9e3dbd75
romainmenke <romainmenke@gmail.com>
2016-12-31 16:33:50 +0100

    Handler + HandlerFunc

--- pusher.go
+++ pusher.go
@@ -2,9 +2,29 @@ package pusher
 
 import "net/http"
 
-// Handler wraps a http.HandlerFunc.
+type pushHandler struct {
+	handlerFunc http.HandlerFunc
+}
+
+func (h *pushHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.handlerFunc(w, r)
+}
+
+// Handler wraps a http.Handler.
 // It will automatically generate Push Promises
-func Handler(handler func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+func Handler(handler http.Handler) http.Handler {
+	return &pushHandler{
+		handlerFunc: newHandlerFunc(handler.ServeHTTP),
+	}
+}
+
+// HandlerFunc wraps a http.HandlerFunc.
+// It will automatically generate Push Promises
+func HandlerFunc(handlerFunc http.HandlerFunc) http.HandlerFunc {
+	return newHandlerFunc(handlerFunc)
+}
+
+func newHandlerFunc(handler func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		defer handler(w, r)
--- pusher_test.go
+++ pusher_test.go
@@ -6,7 +6,7 @@ func ExampleHandler() {
 
 	// Pusher wraps around the static file HandlerFunc
 	http.HandleFunc("/",
-		Handler(http.FileServer(http.Dir("./cmd/static")).ServeHTTP),
+		Handler(http.FileServer(http.Dir("./cmd/static"))).ServeHTTP,
 	)
 
 	err := http.ListenAndServeTLS(":4430", "cmd/localhost.crt", "cmd/localhost.key", nil)
