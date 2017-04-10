c5a529d731bb87d0bfc8c0fdce63e0e517d0586b
Romain Menke <romainmenke@gmail.com>
2017-04-10 18:30:27 +0200

    lots of comments

--- link/const.go
+++ link/const.go
@@ -4,11 +4,15 @@ const (
 	// Get Method
 	Get = "GET"
 	// GoH2Pushed is the header key used to transmit Pushed Link header values
-	GoH2Pushed               = "Go-H2-Pushed"
-	headerAmountLimit        = 64
-	headerLengthLimit        = 1024
+	GoH2Pushed = "Go-H2-Pushed"
+	// headerAmountLimit is the max number of header values parsed
+	headerAmountLimit = 64
+	// headerLengthLimit is the max length of a header value, longer values are skipped
+	headerLengthLimit = 1024
+	// http2ErrPushLimitReached is the err returned when too many pushes were send : https://golang.org/src/net/http/h2_bundle.go
 	http2ErrPushLimitReached = "http2: push would exceed peer's SETTINGS_MAX_CONCURRENT_STREAMS"
-	http2ErrRecursivePush    = "http2: recursive push not allowed"
+	// http2ErrRecursivePush is the err returned when a recursive push was send : https://golang.org/src/net/http/h2_bundle.go
+	http2ErrRecursivePush = "http2: recursive push not allowed"
 	// Link Header Key
 	Link = "Link"
 	// NoPush directive
--- link/link_go18.go
+++ link/link_go18.go
@@ -8,14 +8,20 @@ import "net/http"
 func Handler(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
+		// If CanPush returns false, use the input handler.
+		// Else -> wrap it.
 		if !CanPush(w, r) {
 			handler.ServeHTTP(w, r)
 			return
 		}
 
+		// Get a responseWriter from the sync.Pool.
 		var rw = getResponseWriter(w, r)
+		// defer close the responseWriter.
+		// This returns it to the sync.Pool and zeroes all values and pointers.
 		defer rw.close()
 
+		// handle.
 		handler.ServeHTTP(rw, r)
 
 	})
@@ -24,19 +30,24 @@ func Handler(handler http.Handler) http.Handler {
 // CanPush checks if the Request is Pushable and the ResponseWriter supports H2 Push.
 func CanPush(w http.ResponseWriter, r *http.Request) bool {
 
+	// Only GET requests should trigger Pushes.
 	if r.Method != Get {
 		return false
 	}
 
+	// Push is only supported from HTTP2.0.
 	if r.ProtoMajor < 2 {
 		return false
 	}
 
+	// The http.ResponseWriter has to be http.Pusher.
 	_, ok := w.(http.Pusher)
 	if !ok {
 		return false
 	}
 
+	// The request must not be proxied.
+	// Proxies might not support forwarding Pushes.
 	if r.Header.Get(XForwardedFor) != "" {
 		return false
 	}
@@ -47,51 +58,72 @@ func CanPush(w http.ResponseWriter, r *http.Request) bool {
 // InitiatePush parses Link Headers of a response to generate Push Frames.
 func InitiatePush(w *responseWriter) { // 0 allocs
 
+	// Nil checks, these might be redundant.
 	if w == nil || w.request == nil {
 		return
 	}
 
+	// Cast the wrapper http.ResponseWriter to a http.Pusher.
 	pusher, ok := w.ResponseWriter.(http.Pusher)
 	if !ok {
 		return
 	}
 
+	// Get the Link Header values from the Response Header.
 	linkHeaders, ok := w.Header()[Link]
 	if !ok {
 		return
 	}
 
+	// splitIndex is used to seperate Link and Push values without creating a new []string{}.
 	var splitIndex int
+
 PUSH_LOOP:
 	for index, link := range linkHeaders {
 
+		// Limit the number of values parsed.
+		// This is not based on how many are eventually pushed.
 		if index > headerAmountLimit {
 			break PUSH_LOOP
 		}
 
+		// Parse the Link Header Value.
+		// This will return either an empty string or a relative url.
+		// When not empty -> Push.
 		pushLink := parseLinkHeader(link)
 		if pushLink != "" {
 
+			// Attempt to send a Push.
+			// Pass the original Request Headers by reference.
 			err := pusher.Push(pushLink, &http.PushOptions{
 				Header: w.request.Header,
 			})
+
+			// Handle Push err.
 			if err != nil {
 				switch err.Error() {
+				// No more pushes can be send.
 				case http2ErrPushLimitReached:
 					break PUSH_LOOP
+					// No more pushes can be send.
 				case http2ErrRecursivePush:
 					break PUSH_LOOP
 				default:
+					// Something went wrong, but maybe nothing serious. Try another Push.
 					continue PUSH_LOOP
 				}
 			}
 
+			// Swap two header values.
+			// This will group all Pushed values to the front of the slice.
 			linkHeaders[splitIndex], linkHeaders[index] = linkHeaders[index], linkHeaders[splitIndex]
 			splitIndex++
 		}
 	}
 
-	w.ResponseWriter.Header()[Link] = linkHeaders[splitIndex:]
+	// Move the pushed values to a new Key to prevent the browser from requesting it.
 	w.ResponseWriter.Header()[GoH2Pushed] = linkHeaders[:splitIndex]
+	// Update 'Link' with the remaining values.
+	w.ResponseWriter.Header()[Link] = linkHeaders[splitIndex:]
 
 }
