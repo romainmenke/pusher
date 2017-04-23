abb22238169bd5f46b55ba9645200430b95d670c
Romain Menke <romainmenke@gmail.com>
2017-04-23 14:31:35 +0200

    update travis.yml & test script

--- go.test.sh
+++ go.test.sh
@@ -7,31 +7,36 @@ if [[ $TRAVIS_GO_VERSION == 1.6* ]]
 then
 
   for d in $(go list ./... | grep -v vendor | grep -v http2 | grep -v example); do
-      go test -v -race $d
+    go test -v -race -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
   done
 
 elif [[ $TRAVIS_GO_VERSION == 1.7* ]]
 then
 
   for d in $(go list ./... | grep -v vendor | grep -v http2 | grep -v example); do
+    go test -v -race -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+  done
+
+else
+
+  for d in $(go list ./... | grep -v vendor); do
       go test -v -race $d
   done
 
-elif [[ $TRAVIS_GO_VERSION == 1.8* ]]
-then
-
-  for d in $(go list ./... | grep -v vendor | grep -v example); do
+  for d in $(go list ./... | grep -v vendor | grep -v http2 | grep -v example); do
       go test -v -race -coverprofile=profile.out -covermode=atomic $d
       if [ -f profile.out ]; then
           cat profile.out >> coverage.txt
           rm profile.out
       fi
-  done
-
-else
-
-  for d in $(go list ./... | grep -v vendor | grep -v example); do
-      go test -v -race $d
   done
 
 fi
