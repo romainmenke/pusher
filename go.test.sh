4f5f9c884967371eb59a8354e56534c1810701dd
Romain Menke <romainmenke@gmail.com>
2017-04-23 18:04:50 +0200

    test cov

--- go.test.sh
+++ go.test.sh
@@ -1,29 +1,21 @@
 #!/usr/bin/env bash
 
 set -e
+echo "" > coverage.txt
 
 if [[ $TRAVIS_GO_VERSION == 1.6* ]]
 then
 
   for d in $(go list ./... | grep -v vendor | grep -v http2 | grep -v example); do
-    go test -v -race $d
+      go test -v -race -coverprofile=profile.out -covermode=atomic $d
+      if [ -f profile.out ]; then
+          cat profile.out >> coverage.txt
+          rm profile.out
+      fi
   done
 
 elif [[ $TRAVIS_GO_VERSION == 1.7* ]]
 then
-
-  for d in $(go list ./... | grep -v vendor | grep -v http2 | grep -v example); do
-    go test -v -race $d
-  done
-
-elif [[ $TRAVIS_GO_VERSION == 1.8* ]]
-then
-
-  for d in $(go list ./... | grep -v vendor); do
-      go test -race $d
-  done
-
-  echo "" > coverage.txt
 
   for d in $(go list ./... | grep -v vendor | grep -v http2 | grep -v example); do
       go test -v -race -coverprofile=profile.out -covermode=atomic $d
@@ -36,7 +28,15 @@ then
 else
 
   for d in $(go list ./... | grep -v vendor); do
-      go test -v -race $d
+      go test -race $d
+  done
+
+  for d in $(go list ./... | grep -v vendor | grep -v http2 | grep -v example); do
+      go test -v -race -coverprofile=profile.out -covermode=atomic $d
+      if [ -f profile.out ]; then
+          cat profile.out >> coverage.txt
+          rm profile.out
+      fi
   done
 
 fi
