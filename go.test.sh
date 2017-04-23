8c18a017f42566462dc5f46580f66f6919216bfc
Romain Menke <romainmenke@gmail.com>
2017-04-23 17:22:17 +0200

    only report coverage for go18

--- go.test.sh
+++ go.test.sh
@@ -7,25 +7,18 @@ if [[ $TRAVIS_GO_VERSION == 1.6* ]]
 then
 
   for d in $(go list ./... | grep -v vendor | grep -v http2 | grep -v example); do
-    go test -v -race -coverprofile=profile.out -covermode=atomic $d
-    if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
-        rm profile.out
-    fi
+    go test -v -race $d
   done
 
 elif [[ $TRAVIS_GO_VERSION == 1.7* ]]
 then
 
   for d in $(go list ./... | grep -v vendor | grep -v http2 | grep -v example); do
-    go test -v -race -coverprofile=profile.out -covermode=atomic $d
-    if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
-        rm profile.out
-    fi
+    go test -v -race $d
   done
 
-else
+elif [[ $TRAVIS_GO_VERSION == 1.7* ]]
+then
 
   for d in $(go list ./... | grep -v vendor); do
       go test -v -race $d
@@ -37,6 +30,12 @@ else
           cat profile.out >> coverage.txt
           rm profile.out
       fi
+  done
+
+else
+
+  for d in $(go list ./... | grep -v vendor); do
+      go test -v -race $d
   done
 
 fi
