7a6bb7a57baa15919e017cce1eed0bb64ff4560b
Romain Menke <romainmenke@gmail.com>
2017-04-23 14:14:22 +0200

    version specific tests

--- go.test.sh
+++ go.test.sh
@@ -3,8 +3,8 @@
 set -e
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic $d
+for d in $(go list ./... | grep -v vendor | grep -v http2 | grep -v example); do
+    go test -v -race -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
